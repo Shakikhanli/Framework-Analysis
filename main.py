96000264de5a99c3f14b83d99ad2e7a3881a87e2
Ulvi Shakikhanli <shekikhanli@gmail.com>
2023-01-19 16:25:50 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import json
+import os
+import pandas as pd
+
+address_folder_mono = "D://GDrive/Mono"
+address_folder_multi = "D://GDrive/Multi"
+
+""" This loop designed to get some basic information about Mono repositories"""
+
+df_mono = pd.DataFrame(
+    columns=['Repo name', 'User name', 'Team size', 'Langauge', 'Star count', 'Fork count', 'Commit count', 'Amount of active days'])
+
+dict_list = []
+
+# Mono repositories
+# Same code also can be implemented to Multi repositories after few changes
+for each_file in os.listdir(address_folder_mono):
+    if '.json' in each_file:
+        with open(address_folder_mono + '/' + each_file, 'r', encoding="utf8") as file:
+            file_content = json.load(file)
+
+            dict_mono = {'Repo name': '', 'User name': '', 'Team size': '', 'Langauge': '', 'Star count': '',
+                         'Fork count': '', 'Commit count': '', 'Amount of active days': ''}
+
+            dict_mono['Repo name'] = file_content['repo_name']
+            dict_mono['User name'] = file_content['user_name']
+            dict_mono['Team size'] = file_content['size']
+            dict_mono['Langauge'] = file_content['languages']
+            dict_mono['Star count'] = file_content['star_count']
+            dict_mono['Fork count'] = file_content['fork_count']
+            dict_mono['Commit count'] = len(file_content['commits'])
+            dict_mono['Amount of active days'] = len(file_content['activity'])
+
+            dict_list.append(dict_mono)
+
+df_mono = pd.DataFrame.from_records(dict_list)
+
+print(df_mono.head(10))
+
+
+
+
+
